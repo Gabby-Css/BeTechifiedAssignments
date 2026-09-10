65e06b6ffaf9869aa8169223e4f056197269bf4d
LILGABBY <yawbenarkuu@gmail.com>
2026-09-10 11:49:01 +0000

    Add files via upload

---
+++ Intro.js
@@ -0,0 +1,8 @@
+// intro.js
+const name = "Gabriel Benarkuu";
+const age = 19;
+const favoriteLanguage = "JavaScript"; 
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + favoriteLanguage);
+
